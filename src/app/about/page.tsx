2dcdad6aa3cf0b6c7bb5f8db13fd1975c85a9e79
Destanto <destantomy8@gmail.com>
2024-09-09 14:17:46 +0700

    the dif: layout will bring the state, template will create new instance

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,7 @@
-import Navbar from "./navbar";
-
 export default function About () {
     return(
         <div>
-            <Navbar></Navbar>
-            <h1>Test about</h1>
+            <h1>This from about</h1>
         </div>
     )
 }
