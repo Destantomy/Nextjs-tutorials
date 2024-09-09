45881319b82572ed479c6dc0e467c400f4ee545a
Destanto <destantomy8@gmail.com>
2024-09-09 10:51:03 +0700

    getting started

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./navbar";
+
+export default function About () {
+    return(
+        <div>
+            <Navbar></Navbar>
+            <h1>Test about</h1>
+        </div>
+    )
+}
