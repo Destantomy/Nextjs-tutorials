d76ef2d97885cc141f0f7882b4d7d43aa1daf336
Destanto <destantomy8@gmail.com>
2024-09-12 11:37:46 +0700

    05-api-route-handlers

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,20 @@
+"use client";
+
 import Link from "next/link";
 
 export default function Login() {
+
+	const handleLogin = (e: any) => {
+		e.preventDefault();
+		fetch('/api/auth/login', {
+				method: 'POST',
+				body: JSON.stringify({
+					email: e.currentTarget.email.value,
+					password: e.currentTarget.password.value
+			})
+		});
+	}
+
     return(
         
     <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
@@ -12,26 +26,28 @@ export default function Login() {
 				<div>
 					<h1 className="text-2xl font-semibold">Login to our platform</h1>
 				</div>
+					<form className="space-y-6" onSubmit={(e) => handleLogin(e)}>
 				<div className="divide-y divide-gray-200">
-					<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-						<div className="relative">
-							<input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
-							<label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+						<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+							<div className="relative">
+								<input autoComplete="off" id="email" name="email" type="text" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Email address" />
+								<label htmlFor="email" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Email Address</label>
+							</div>
+							<div className="relative">
+								<input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
+								<label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
+							</div>
+							<div className="relative">
+								<button className="bg-blue-500 text-white rounded-md px-2 py-1">Login</button>
+							</div>
+							<div>
+								<p>Did not have any account? 
+								</p>
+									<Link href='/register' className="text-blue-600">Register here !</Link>
+							</div>
 						</div>
-						<div className="relative">
-							<input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600" placeholder="Password" />
-							<label htmlFor="password" className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm">Password</label>
-						</div>
-						<div className="relative">
-							<button className="bg-blue-500 text-white rounded-md px-2 py-1">Login</button>
-						</div>
-                        <div>
-                            <p>Did not have any account? 
-                            </p>
-                                <Link href='/register' className="text-blue-600">Register here !</Link>
-                        </div>
-					</div>
 				</div>
+					</form>
 			    </div>
 		    </div>
 	    </div>
