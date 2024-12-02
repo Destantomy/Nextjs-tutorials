c8c627922d67747da51f6cc9840d55233f9e983c
Destanto <destantomy8@gmail.com>
2024-12-02 11:42:21 +0700

    19-seo-and-metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: 'Home',
+  description: 'Next.js tutorial from scratch',
+  authors: [{
+    name: 'desta',
+    url: 'https://github.com/destantomy'
+  }],
+  icons: {
+    icon: '/images/icon.png',
+  },
+  openGraph: {
+    title: 'Home | from openGraph'
+  }
+};
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
